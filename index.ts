6497daf18d8b8d4be54bfb9e94705d3eb69c277f
F-crystal9 <160336289+F-crystal9@users.noreply.github.com>
2024-06-26 19:58:36 +0500

    first commit

--- index.ts
+++ index.ts
@@ -1,40 +1,24 @@
- import inquirer from "inquirer";
-
+import inquirer from "inquirer";
 const answer = await inquirer.prompt([
-  { message: "Enter First Number", type: "number", name :"Firstnumber" },
-  { message :"Enter Second Number", type:"number", name:"Secondnumber"},
-{
-
-message: "Select one of the operators to perform operation", type:"list", name:"operator",
-choices: ["Addition","Subtraction","Multiplication","Division"],
-
-},
-
-
+  { message: "Enter First Number", type: "number", name: "Firstnumber" },
+  { message: "Enter Second Number", type: "number", name: "Secondnumber" },
+  {
+    message: "Select one of the operators to perform operation",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
 ]);
 
 // conditional statement
-if(answer.operator === "Addition"){
-console.log( answer.Firstnumber + answer.Secondnumber)
-
+if (answer.operator === "Addition") {
+  console.log(answer.Firstnumber + answer.Secondnumber);
+} else if (answer.operator === "Subtraction") {
+  console.log(answer.Firstnumber - answer.Secondnumber);
+} else if (answer.operator === "Multiplication") {
+  console.log(answer.Firstnumber * answer.Secondnumber);
+} else if (answer.operator === "Division") {
+  console.log(answer.Firstnumber / answer.Secondnumber);
+} else {
+  console.log("Please selecr a valid operator");
 }
-
-else if(answer.operator === "Subtraction"){
-    console.log( answer.Firstnumber - answer.Secondnumber)
-    
-    }
-
-    else if(answer.operator === "Multiplication"){
-        console.log( answer.Firstnumber * answer.Secondnumber)
-        
-        }
-
-        else if(answer.operator === "Division"){
-            console.log( answer.Firstnumber / answer.Secondnumber)
-            
-            }
-
-            else{
-              console.log("Please selecr a valid operator")
-
-            }
